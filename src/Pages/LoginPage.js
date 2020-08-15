dc7d75b34749b65186d3d2ba6e7841a84d3f4386
Neerajistheboss <33841268+Neerajistheboss@users.noreply.github.com>
2020-08-15 21:16:39 +0530

    local

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -99,7 +99,7 @@ const LoginPage = (props) => {
 											{/* Login with facebook and google */}
 
 											<a
-												href='https://yuver.herokuapp.com/api/v1/auth/oauth/google'
+												href='http://localhost:5000/api/v1/auth/oauth/google'
 												className='p-1 rounded-lg d-flex my-2 text-light'
 												style={{
 													backgroundColor: '#4267B2',
@@ -113,7 +113,7 @@ const LoginPage = (props) => {
 												</p>
 											</a>
 											<a
-												href='https://yuver.herokuapp.com/api/v1/auth/oauth/google'
+												href='http://localhost:5000/api/v1/auth/oauth/google'
 												className='p-1 rounded-lg d-flex  my-2 text-light'
 												style={{
 													backgroundColor: '#1976D2',
