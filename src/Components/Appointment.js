6ed0d61190a830676abf67f9ff588a23d81befed
Neerajistheboss <33841268+Neerajistheboss@users.noreply.github.com>
2020-08-28 23:56:02 +0530

    appointment status will show awaiting payment if there is delay from payment gateway , earlier it showed failed

--- src/Components/Appointment.js
+++ src/Components/Appointment.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import moment from 'moment'
 const Appointment = (props) => {
+	let status, color
+	if (props.appointment.status == 'awaitingPayment') {
+		status = 'AwaitingPayment'
+		color = 'bg-warning'
+	} else if (props.appointment.status == 'confirmed') {
+		status = 'Confirmed'
+		color = 'bg-success'
+	} else {
+		status = 'Failed'
+		color = 'bg-danger'
+	}
+
 	return (
 		<div
 			className=' rounded d-flex  justify-content-around m-3'
@@ -14,25 +26,20 @@ const Appointment = (props) => {
 				<h6 className='d-inline-block m-0'>
 					{props.appointment.hospital.name}
 				</h6>
-				{props.appointment.status != 'awaitingPayment' ? (
-					<React.Fragment>
-						<h6 className='bg-warning p-1 rounded'>
-							Status:{props.appointment.status}
-						</h6>
-						<h6 className='my-1 m-0'>
-							{moment(
-								`${props.appointment.dateTime.date} ${props.appointment.dateTime.time}`
-							).calendar()}
-						</h6>
-					</React.Fragment>
-				) : (
-					<h6 className='bg-danger p-1 rounded'>Status:{'Payment Declined'}</h6>
-				)}
+
+				<React.Fragment>
+					<h6 className={`${color} p-1 rounded`}>Status:{status}</h6>
+					<h6 className='my-1 m-0'>
+						{moment(
+							`${props.appointment.dateTime.date} ${props.appointment.dateTime.time}`
+						).calendar()}
+					</h6>
+				</React.Fragment>
 
 				{/* <h6 className=''>{moment(props.appointment.time).format('LT')}</h6> */}
 			</div>
 			<div className='d-flex flex-column justify-content-around '>
-				{props.appointment.status !== 'confirmed' && (
+				{props.appointment.status == 'cancelled' && (
 					<button className='btn rounded btn-danger m-0'>Failed</button>
 				)}
 			</div>
