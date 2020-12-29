23764ea2350f96aaf1bdeb5723de48d096fd5113
Neerajistheboss <neerajistheboss@gmail.com>
2020-12-29 11:42:20 +0530

    added react-https-redirect package

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HttpsRedirect from 'react-https-redirect';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<HttpsRedirect><App /></HttpsRedirect>,document.getElementById('root'));
